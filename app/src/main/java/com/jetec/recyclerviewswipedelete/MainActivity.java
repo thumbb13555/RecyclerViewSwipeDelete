c7859a1fde13e35671d48dbd96c722d019cbf739
NoahLiu <thumbb13555@gmail.com>
2019-12-29 00:02:06 +0800

    Initial commit

---
+++ app/src/main/java/com/jetec/recyclerviewswipedelete/MainActivity.java
@@ -0,0 +1,129 @@
+package com.jetec.recyclerviewswipedelete;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+public class MainActivity extends AppCompatActivity {
+    String[] A2J = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"};
+    ArrayList<String> arrayList = new ArrayList<>();
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //生成資料
+        for (int i = 0; i < A2J.length; i++) {
+            arrayList.add(A2J[i]);
+        }
+
+        //設置RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
+        //設置RecyclerView滑動事件
+        recyclerViewAction(recyclerView, arrayList, myAdapter);
+
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.textView_item);
+            }
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.textView.setText(arrayList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return arrayList.size();
+        }
+
+
+    }
+
+    private void recyclerViewAction(RecyclerView recyclerView, final ArrayList<String> choose, final MyAdapter myAdapter) {
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+                return makeMovementFlags(ItemTouchHelper.UP | ItemTouchHelper.DOWN
+                        , ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+            }
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView
+                    , @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int position_dragged = viewHolder.getAdapterPosition();
+                int position_target = target.getAdapterPosition();
+                Collections.swap(choose, position_dragged, position_target);
+                myAdapter.notifyItemMoved(position_dragged, position_target);
+
+                if (choose.contains("K") && choose.indexOf("K") != choose.size() - 1) {
+                    Collections.swap(choose, position_target, position_dragged);
+                    myAdapter.notifyItemMoved(position_target, position_dragged);
+                }
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                switch (direction) {
+                    case ItemTouchHelper.LEFT:
+                    case ItemTouchHelper.RIGHT:
+                        choose.remove(position);
+                        myAdapter.notifyItemRemoved(position);
+                        break;
+                }
+            }
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                new RecyclerViewSwipeDecorator.Builder(c,recyclerView,viewHolder,dX,dY,actionState,isCurrentlyActive)
+                        .addBackgroundColor(ContextCompat.getColor(MainActivity.this,android.R.color.holo_red_dark))
+                        .addActionIcon(R.drawable.ic_delete_black_24dp)
+                        .create()
+                        .decorate();
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        });
+        helper.attachToRecyclerView(recyclerView);
+
+    }
+
+}
